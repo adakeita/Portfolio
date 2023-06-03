b826d522ab1d246d2d4f6ff5e4ae4164ee2ba6cc
Ada Keita <mathildebrakstad@gmail.com>
2023-06-03 18:15:42 +0200

    first commit

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,9 +1,27 @@
 export const handleMenu = () => {
     const navigation = document.querySelector(".nav-wrapper");
     const menuIcon = document.querySelector(".menu-icon");
+    const navLinks = document.querySelectorAll(".nav-item a");
 
     menuIcon.addEventListener("click", () => {
         navigation.classList.toggle("open");
         navigation.classList.toggle("closed");
+
+        // Change the menu icon depending on whether the menu is open or closed
+        if (navigation.classList.contains("open")) {
+            menuIcon.src = "icons/logout.png";
+        } else {
+            menuIcon.src = "icons/bar.png";
+        }
+    });
+
+    // Add active class to current page link
+    const currentPage = window.location.href;
+    navLinks.forEach(link => {
+        if (link.href === currentPage) {
+            link.classList.add("active");
+        } else {
+            link.classList.remove("active");
+        }
     });
 };
